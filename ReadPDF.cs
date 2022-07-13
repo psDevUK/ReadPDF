8819a9b874467d73c220104bc721a8538465d452
Adam <adambacon1@hotmail.com>
2022-07-13 20:47:24 +0100

    Add files via upload

---
+++ ReadPDF.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Management.Automation;
+using UglyToad.PdfPig;
+using UglyToad.PdfPig.Content;
+namespace ReadPDF
+
+{
+    [Cmdlet(VerbsData.Import, "PDFFile")]
+    public class Program : PSCmdlet
+    {
+        [Parameter(Mandatory = true)]
+        public string Path { get; set; }
+        protected override void EndProcessing()
+        {
+            using (PdfDocument document = PdfDocument.Open(Path))
+            {
+                foreach (Page page in document.GetPages())
+                {
+                    string pageText = page.Text;
+
+                    foreach (Word word in page.GetWords())
+                    {
+                        WriteObject(word.Text);
+                    }
+                }
+            }
+
+        }
+    }
+}
